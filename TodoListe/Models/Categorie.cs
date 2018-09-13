e8141d92f2fa104d77c940e45c2ed923f523a884
x <x@x.com>
2018-09-13 16:40:53 +0200

    Creation des classe taches et Categorie

---
+++ TodoListe/Models/Categorie.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TodoListe.Models
+{
+    public class Categorie
+    {
+        public int ID { get; set; }
+
+        public string Nom { get; set; }
+    }
+}
---
+++ TodoListe/Models/Tache.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TodoListe.Models
+{
+    public class Tache
+    {// Ce que l'ion a dans une tahe :
+        public int ID { get; set; }
+
+        public string Nom { get; set; }
+
+        public DateTime Datefin { get; set; }
+
+        public bool Statut { get; set; }
+
+        public int Priorite { get; set; }
+
+    }
+}
