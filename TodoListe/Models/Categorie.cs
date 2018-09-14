f073cf08ba0ace7a94fa00dff53f7993b1d31980
Ben <x@x.com>
2018-09-14 10:17:26 +0200

    Remise en place d'Entity via Nuget

--- TodoListe/Models/Categorie.cs
+++ TodoListe/Models/Categorie.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Data.Entity;
-/* Le Polymorphisme : */
+/* Le Polymorphisme : Permet d'instancier un objet à partir d'une forme et lui en donner une autre
+ Un Type est donc une forme*/
 
 
 
