975d8709aca1d3b6eb6eb953b20fbcd9bfd5835d
Eternal-Ben <Eternalst@live.fr>
2018-09-17 07:23:20 +0200

    Ajout commentaaire repèrs pour sur la creation de model de table

--- TodoListe/Models/Tache.cs
+++ TodoListe/Models/Tache.cs
@@ -11,15 +11,15 @@ namespace TodoListe.Models
     public class Tache
     {// Ce que l'on a dans une tache :
         public int ID { get; set; }
-        [Required]
-        [StringLength(30)]
+        [Required] // "Required" precise que sa presence est obligatoir
+        [StringLength(30)] //precise le nombre de d'entier max precisé par le diag de class ou sprintBackLog
 
         public string Nom { get; set; }
         [StringLength(250)]
 
         public string Description { get; set; }
 
-        public DateTime ? Datefin { get; set; }
+        public DateTime ? Datefin { get; set; } // s'ecrit toujours de ce format, DateTime"?" Datefin
         [Required]
 
         public bool Statut { get; set; }
@@ -27,7 +27,7 @@ namespace TodoListe.Models
         public int Priorite { get; set; }
 
         public int CategorieID { get; set; }
-        [ForeignKey("CategorieID")] // pour la creation d'une clee etrangere; [ForeignKey("nom de la ForeignKey")], cela correspondant a la clee primaire de la table Categorie
+        [ForeignKey("CategorieID")] // pour la creation d'une clee etrangere; [ForeignKey("nom de la ForeignKey")], cela correspondant a la clee primaire de la table Categorie et permet la relation entre ces tables.
 
         public Categorie Categorie { get; set; }
     }
