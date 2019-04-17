39d6ae65b3e6b066a5c563e41b43332b743cf52a
Ben.VisualStd <lst.thierry@gmail.com>
2019-04-17 08:31:20 +0200

    ??

--- TodoList/Models/Categorie.cs
+++ TodoList/Models/Categorie.cs
@@ -19,7 +19,7 @@ namespace TodoList.Models
         /* Ligne suivante indique les propriétés de la table qui precise que sa presence est requise avec un string de taille de 20char max et nom de la colonne est nommé "Name"*/
         [Required] // on precise q'l sont les parametre, ci dessous, que l'on souhaite avoir 
         [StringLength(20)] // on limit le string à 20 char
-        [Column("Name")] // selection de la colonne Name qui est la propriete
+        //[Column("Name")] // selection de la colonne Name qui est la propriete
         public string Nom { get; set; }
     }
 }
